babdcaca60fc4c061dc7196da7d68faf925bad19
0xSenseii <1upHax1010@gmail.com>
2021-07-10 16:41:13 +0200

    first commit

---
+++ src/script.js
@@ -0,0 +1,102 @@
+import './style.css'
+import * as THREE from 'three'
+
+// Loading textures
+const textureLoader = new THREE.TextureLoader()
+const normalTexture = textureLoader.load('../textures/NormalMap.png')
+const world = textureLoader.load('../textures/world.png')
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+// Objects
+const geometry = new THREE.SphereBufferGeometry(.7, 64, 64);
+
+// Materials
+const material = new THREE.MeshStandardMaterial({ map: world });
+material.color = new THREE.Color()
+material.normalMap = normalTexture
+
+// Mesh
+const sphere = new THREE.Mesh(geometry, material)
+scene.add(sphere)
+
+// Lights
+const pointLight = new THREE.PointLight(0xffffff, 1)
+pointLight.position.set(0, 2.5, 2.2)
+scene.add(pointLight)
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () => {
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 0
+camera.position.y = 0
+camera.position.z = 2
+scene.add(camera)
+
+// Controls
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    alpha: true
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+
+const clock = new THREE.Clock()
+
+const tick = () => {
+
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update objects
+    // sphere.rotation.x = .3 * elapsedTime
+    sphere.rotation.y = 0.3 * elapsedTime
+
+    // Update Orbital Controls
+    // controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+
+}
+
+tick()
